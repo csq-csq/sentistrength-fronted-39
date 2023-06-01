859676452e81f4149d49466b4adcef5d80f7057c
ChenSiQi <201250021@smail.nju.edu.cn>
2023-06-01 15:54:58 +0800

    引入echarts并添加了两个表格

--- vue/src/main.js
+++ vue/src/main.js
@@ -5,10 +5,12 @@ import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import './assets/global.css';
 import request from "@/utils/request";
+import * as echarts from 'echarts';
 
 Vue.config.productionTip = false;
 Vue.use(ElementUI);
 Vue.prototype.request=request();
+Vue.prototype.$echarts = echarts
 new Vue({
   router,
   render: h => h(App)
--- vue/src/views/Visualization.vue
+++ vue/src/views/Visualization.vue
@@ -1,12 +1,134 @@
 <template>
   <div>
-    数据可视化
+    <div id="ChartA" :style="{width: '100%', height: '1000px'}" />
+    <el-divider></el-divider>
+    <button @click="fetchData">Get Data</button>
+    <div id="ChartB" style="width: 600px;height: 400px;"></div>
   </div>
 </template>
 
 <script>
+import request from "@/utils/request";
+
+
 export default {
-  name: "Visualization"
+  name: "Visualization",
+  data() {
+    return {
+      chart: null,
+    }
+  },
+  mounted() {
+    this.drawLine()
+  },
+  methods: {
+    async fetchData() {
+      const ChartB = this.$echarts.init(document.getElementById('ChartB'))
+      await request
+          .get('/getCSV')
+          .then(successResponse => {
+            if(successResponse.code==200){
+              const option = this.createChartOption(successResponse.data)
+              ChartB.setOption(option)
+            }else{
+              console.log(successResponse.message)
+            }
+          })
+          .catch(failResponse => {
+            console.log(failResponse.message)
+          })
+    },
+    createChartOption(data) {
+      // convert data to ECharts option here
+      // this is just an example, you need to adjust it to fit your real data structure and visualization requirement
+      return {
+        xAxis: {
+          type: 'category',
+          data: data.map(item => item.date),
+        },
+        yAxis: {
+          type: 'value',
+        },
+        series: [{
+          data: data.map(item => item.value),
+          type: 'bar',
+        }],
+      }
+    },
+    drawLine() {
+      // 基于准备好的dom，初始化echarts实例
+      const ChartA = this.$echarts.init(document.getElementById('ChartA'));
+      ChartA.setOption({
+        //官网实例代码，如下图
+        title: {
+          text: 'Rainfall vs Evaporation',
+          subtext: 'Fake Data'
+        },
+        tooltip: {
+          trigger: 'axis'
+        },
+        legend: {
+          data: ['Rainfall', 'Evaporation']
+        },
+        toolbox: {
+          show: true,
+          feature: {
+            dataView: { show: true, readOnly: false },
+            magicType: { show: true, type: ['line', 'bar'] },
+            restore: { show: true },
+            saveAsImage: { show: true }
+          }
+        },
+        calculable: true,
+        xAxis: [
+          {
+            type: 'category',
+            // prettier-ignore
+            data: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+          }
+        ],
+        yAxis: [
+          {
+            type: 'value'
+          }
+        ],
+        series: [
+          {
+            name: 'Rainfall',
+            type: 'bar',
+            data: [
+              2.0, 4.9, 7.0, 23.2, 25.6, 76.7, 135.6, 162.2, 32.6, 20.0, 6.4, 3.3
+            ],
+            markPoint: {
+              data: [
+                { type: 'max', name: 'Max' },
+                { type: 'min', name: 'Min' }
+              ]
+            },
+            markLine: {
+              data: [{ type: 'average', name: 'Avg' }]
+            }
+          },
+          {
+            name: 'Evaporation',
+            type: 'bar',
+            data: [
+              2.6, 5.9, 9.0, 26.4, 28.7, 70.7, 175.6, 182.2, 48.7, 18.8, 6.0, 2.3
+            ],
+            markPoint: {
+              data: [
+                { name: 'Max', value: 182.2, xAxis: 7, yAxis: 183 },
+                { name: 'Min', value: 2.3, xAxis: 11, yAxis: 3 }
+              ]
+            },
+            markLine: {
+              data: [{ type: 'average', name: 'Avg' }]
+            }
+          }
+        ]
+      })
+    }
+  }
 }
 </script>
 
